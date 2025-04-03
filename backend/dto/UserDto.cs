04e6712babb980bb8a3b4175afe9462e820cec63
Maxim <maks.degtearyov25@gmail.com>
2025-04-03 18:39:20 +0300

    Распределил по папкам для читаемости

--- backend/dto/UserDto.cs
+++
@@ -1,8 +0,0 @@
-namespace Mirea.freelance.backend.dto;
-
-public class UserDto
-{
-    public int Id { get; set; }
-    public string Login { get; set; } = string.Empty;
-    public DateTime RegistrationDate { get; set; }
-}
