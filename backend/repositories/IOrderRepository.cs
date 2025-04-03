8478fbc5207b59d5e7ea0d38460c7405f3ad8ed8
Maxim <maks.degtearyov25@gmail.com>
2025-04-03 12:51:51 +0300

    Create IOrderRepository.cs

---
+++ backend/repositories/IOrderRepository.cs
@@ -0,0 +1,30 @@
+using Mirea.freelance.backend.models;
+
+namespace Mirea.freelance.backend.repositories;
+
+public interface IOrderRepository
+{
+    //Получить заказ по его ID
+    Task<Order?> GetByIdAsync(int id);
+    
+    //
+    Task<IEnumerable<Order>> GetAllAsync();
+    
+    //
+    Task AddAsync(Order order);
+    
+    //
+    Task UpdateAsync(Order order);
+    
+    //
+    Task DeleteAsync(Order order);
+    
+    //
+    Task<IEnumerable<Order>> GetOrdersByCompanyIdAsync(int companyProfileId);
+    
+    //
+    Task<IEnumerable<Order>> GetOrdersByFreelancerIdAsync(int freelancerProfileId);
+    
+    //
+    Task<IEnumerable<Order>> GetOrdersAsync();
+}
