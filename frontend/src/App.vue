d57fb3f39001c8186f69f00af707c5ed58a80089
AnnMazur <mazur20ann@yandex.ru>
2025-04-03 13:54:53 +0300

    дизайн главной страницы

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -26,15 +26,28 @@ export default {
 html, body {
   height: 100%;
   margin: 0;
+  
 }
 
 #app {
   display: flex;
   flex-direction: column;
   min-height: 100vh;
+ 
 }
-
 main {
   flex: 1; /* Занимает всё свободное пространство */
+  background-color: #0D0F1A;
+  color: #E0E0E0;
+}
+
+@font-face {
+	font-family: 'BezierSans-Regular';
+	src: url('./fonts/beziersans-regular.eot'); /* IE 9 Compatibility Mode */
+	src: url('./fonts/beziersans-regular.eot?#iefix') format('embedded-opentype'), /* IE < 9 */
+		url('./fonts/beziersans-regular.woff2') format('woff2'), /* Super Modern Browsers */
+		url('./fonts/beziersans-regular.woff') format('woff'), /* Firefox >= 3.6, any other modern browser */
+		url('./fonts/beziersans-regular.ttf') format('truetype'), /* Safari, Android, iOS */
+		url('./fonts/beziersans-regular.svg#beziersans-regular') format('svg'); /* Chrome < 4, Legacy iOS */
 }
 </style>
