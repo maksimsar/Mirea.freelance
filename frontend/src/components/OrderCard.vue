30f3198d08bb7bd5165a80d7cec047170e71ea0a
AnnMazur <mazur20ann@yandex.ru>
2025-04-04 16:27:37 +0300

    авторизация и заказы у студента дизайн

--- frontend/src/components/OrderCard.vue
+++ frontend/src/components/OrderCard.vue
@@ -3,7 +3,7 @@
       <div class="card-body">
         <h5 class="card-title">{{ order.title }}</h5>
         <p class="card-text">{{ order.description }}</p>
-        <button class="btn btn-primary" @click="$emit('respond', order)">Откликнуться</button>
+        <button class="btn" @click="$emit('respond', order)">Откликнуться</button>
       </div>
     </div>
   </template>
@@ -22,23 +22,42 @@
   
   <style scoped>
   .order-card {
-    min-height: 200px; /* Минимальная высота карточки */
+    min-height: 200px; 
+    max-width: 90%;
+
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     padding: 20px;
-    border: 1px solid #ddd; /* Легкая граница */
-    border-radius: 8px; /* Закругленные углы */
+    border-radius: 8px;
+    background-color: #2D3445;
+    color:  #E0E0E0;
+    transition: color 0.3s ease, transform 0.3s ease;
+  }
+
+  .order-card:hover {
+    transform: scale(1.05);
   }
   
   .card-title {
-    font-size: 1.25rem;
-    font-weight: bold;
+    font-size: 1.3rem;
+    font-family: 'BezierSans-Regular';
   }
   
   .card-text {
     flex-grow: 1;
     margin-bottom: 15px; /* Отступ от кнопки */
   }
+
+  .btn {
+  background-color: #00b5c5;
+  color: white;
+
+}
+.btn:hover {
+  background-color:  #037485;
+  color: white;
+}
   </style>
+
   
