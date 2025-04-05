081a7e87752d03100307fa7de350d481b9c87e7f
AnnMazur <mazur20ann@yandex.ru>
2025-04-05 11:24:51 +0300

    отслеживание заказов у студента кривое

--- frontend/src/components/ProgressBar.vue
+++ frontend/src/components/ProgressBar.vue
@@ -18,14 +18,14 @@
   .progress-bar {
     position: relative;
     height: 20px;
-    background-color: #e9ecef;
+    background-color:  #0D0F1A;
     border-radius: 10px;
     overflow: hidden;
     margin: 10px 0;
   }
   .progress {
     height: 100%;
-    background-color: #28a745;
+    background-color: #037485;
     transition: width 0.3s ease;
   }
   .progress-text {
@@ -34,7 +34,8 @@
     left: 50%;
     transform: translateX(-50%);
     font-size: 0.8rem;
-    color: #fff;
+    color: #ffffff;
+    text-shadow: #181B29 1px 1px 2px;
   }
   </style>
   
