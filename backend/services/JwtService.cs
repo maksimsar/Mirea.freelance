bb736fc36473e20ea2d787c01d4678b8f0c90e2e
nastushhh <kolodkinanasty2006@gmail.com>
2025-05-21 15:55:09 +0300

    update generation jwt token

--- backend/services/JwtService.cs
+++ backend/services/JwtService.cs
@@ -5,20 +5,21 @@ using System.Text;
 using Mirea.freelance.backend.models;
 using Microsoft.AspNetCore.Identity;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
 
 namespace Mirea.freelance.backend.services;
 
 public class JwtService
 {
-    private readonly IConfiguration _configuration;
+    private readonly JwtOptions _jwtOptions;
     private readonly UserManager<User> _userManager;
 
-    public JwtService(IConfiguration configuration, UserManager<User> userManager)
+    public JwtService(IOptions<JwtOptions> JwtOptions, UserManager<User> userManager)
     {
-        _configuration = configuration;
+        _jwtOptions = JwtOptions.Value;
         _userManager = userManager;
     }
-
+ 
     public async Task<string> GenerateJwtToken(User user)
     {
         var claims = new List<Claim>
@@ -31,15 +32,16 @@ public class JwtService
         var roles = await _userManager.GetRolesAsync(user);
         claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
 
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]!));
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.Key));
         var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
         var token = new JwtSecurityToken(
-            issuer: _configuration["Jwt:Issuer"],
-            audience: _configuration["Jwt:Audience"],
+            issuer: _jwtOptions.Issuer,
+            audience: _jwtOptions.Audience,
             claims: claims,
             expires: DateTime.Now.AddMinutes(30),
-            signingCredentials: creds);
+            signingCredentials: creds
+        );
 
         return new JwtSecurityTokenHandler().WriteToken(token);
     }
